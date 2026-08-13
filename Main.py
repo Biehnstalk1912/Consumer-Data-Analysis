19e202de380fe2e5db6cc618c6c549af13eadd63
Biehnstalk1912 <Biehnstalk1912@gmail.com>
2026-08-13 17:18:58 -0500

    saving everything
    savin

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-
-
+@app.route('/')
+@app.route('/csv.data')
+def csv_to_html():
+    df = pd.read_csv('product_sales.csv')
+    html_table = df.to_html()
+    return render_template("index.html", table=html_table)
 
 if __name__ == "__main__":
     app.run(debug = True)
