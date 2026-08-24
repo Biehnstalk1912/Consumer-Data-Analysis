59f5b36d3271a44783086487142ca238df0c3746
Biehnstalk1912 <Biehnstalk1912@gmail.com>
2026-08-24 14:34:53 -0500

    Taking a break

--- Main.py
+++ Main.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os 
+import pandas as pd
+import plotly.express as px
 from SQLConversion import parseCSV
+import json
+from sqlalchemy import create_engine
+import plotly.utils
 
+engine = create_engine('mysql+pymysql://root:@localhost/flask_project', echo=True)
 
 app = Flask(__name__)
 
-TABLE_NAME = "orders_table  "
+TABLE_NAME = "orders_table"
 UPLOAD_FOLDER = 'static/files'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    df = pd.read_sql(f'SELECT * FROM {TABLE_NAME}', con=engine)
+    fig = px.bar(df, x='State', y='Quantity', color='Category', barmode='group')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("index.html", graphJSON=graphJSON)
 
 @app.route("/", methods=['POST'])
 def uploadFiles():
--- flask.sql
+++ flask.sql
@@ -1,4 +1,7 @@
+-- Active: 1786998924903@@127.0.0.1@3306@flask_project
+-- Active: 1786998924903@@127.0.0.1@3306@flask_project
 USE flask_project;
-SELECT *
+SELECT `State`, Category, ROUND(AVG(Unit_Price), 2) AS Average_Unit_Price, SUM(Quantity) AS Total_Quantity
 FROM orders_table
-WHERE state = "Minnesota";
+GROUP BY `State`, Category
+ORDER BY `State` DESC;  
