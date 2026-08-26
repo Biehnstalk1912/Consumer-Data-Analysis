1128f3eafbe940b8c75ab3e26e6c55e0cc540088
Biehnstalk1912 <Biehnstalk1912@gmail.com>
2026-08-26 12:02:23 -0500

    Final

--- flask.sql
+++
@@ -1,7 +0,0 @@
--- Active: 1786998924903@@127.0.0.1@3306@flask_project
--- Active: 1786998924903@@127.0.0.1@3306@flask_project
-USE flask_project;
-SELECT `State`, Category, ROUND(AVG(Unit_Price), 2) AS Average_Unit_Price, SUM(Quantity) AS Total_Quantity
-FROM orders_table
-GROUP BY `State`, Category
-ORDER BY `State` DESC;  
